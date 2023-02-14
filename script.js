0e5e15308e003e18cc381064837582d47ed15c34
Hector Hernandez <hectorcreatives08@gmail.com>
2023-02-14 11:48:38 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+export const data = [
+    {
+      questions: "¿Corro para llegar más rápido a donde voy.?",
+      choices: [
+        'Discrepar',
+        'Ligeramente en Desacuerdo',
+        'Ligeramente de Acuerdo',
+        'De Acuerdo',
+      ],
+      personalityType: {
+        "Discrepar": "C",
+        "Ligeramente en Desacuerdo": "I",
+        "Ligeramente de Acuerdo": "S",
+        "De Acuerdo": "D"
+      }
+    },
+    
+   
+  ];
